9908eb1ccd3d94f4f5c4e8612747c235db0b44e5
Van Alphen <24867276@sun.ac.za>
2025-09-23 10:53:13 +0200

    Updated ConnectionSetup script to be more robust

--- Assets/Yanik/Scripts/ConnectionSetup.cs
+++ Assets/Yanik/Scripts/ConnectionSetup.cs
@@ -1,19 +1,35 @@
+using FishNet;
 using FishNet.Managing;
-using UnityEngine;
+using FishNet.Transporting;
 using FishNet.Transporting.Tugboat;
+using UnityEngine;
+using System.Net;
+using System.Linq;
+using TMPro;
 
 public class ConnectionSetup : MonoBehaviour
 {
-  [SerializeField] NetworkManager networkManager;
+  [SerializeField] private NetworkManager networkManager;
   public Canvas mainMenuCanvas;
+  [SerializeField] private GameObject ipInputFieldObject;
+
+  private string IP = "10.255.10.54";
+
+  private void Start()
+  {
+    // Debug connection states
+    InstanceFinder.ClientManager.OnClientConnectionState += (state) => Debug.Log($"Client state: {state}");
+    InstanceFinder.ServerManager.OnServerConnectionState += (state) => Debug.Log($"Server state: {state}");
+  }
 
   // Host is server and client, therefore start both
   public void StartHost()
   {
     var tugboat = (Tugboat)networkManager.TransportManager.Transport;
-    tugboat.SetClientAddress("127.0.0.1");
+    tugboat.SetClientAddress("127.0.0.1"); // Host's client connects to itself
+    tugboat.SetServerBindAddress("0.0.0.0", IPAddressType.IPv4);
     tugboat.SetPort(7770);
-
+    Debug.Log("Starting Host on 10.0.0.1:7770");
     StartServer();
     StartClient();
   }
@@ -22,6 +38,7 @@ public class ConnectionSetup : MonoBehaviour
   public void StartServer()
   {
     networkManager.ServerManager.StartConnection();
+    Debug.Log("Server started");
     HideMenu();
   }
 
@@ -29,19 +46,34 @@ public class ConnectionSetup : MonoBehaviour
   public void StartClient()
   {
     networkManager.ClientManager.StartConnection();
+    Debug.Log("Client connection started");
     HideMenu();
   }
 
   // Set on the Transport to indicate where the Client should connect
-  public void SetIPAddress(string ip)
+  public void SetIPAddress(GameObject ipInputFieldObject)
   {
-    if (!string.IsNullOrEmpty(ip))
+    var tugboat = (Tugboat)networkManager.TransportManager.Transport;
+    if (ipInputFieldObject != null)
     {
-      var tugboat = (Tugboat)networkManager.TransportManager.Transport;
-      tugboat.SetClientAddress(ip);
-      tugboat.SetPort(7770);
-      //networkManager.TransportManager.Transport.SetClientAddress(ip);
+      TMP_InputField inputField = ipInputFieldObject.GetComponent<TMP_InputField>();
+      if (inputField != null && !string.IsNullOrEmpty(inputField.text))
+      {
+        tugboat.SetClientAddress(inputField.text);
+        Debug.Log($"User Input: Changed Host IP to connect to: {inputField.text}");
+      }
+      else
+      {
+        tugboat.SetClientAddress(IP);
+        Debug.LogWarning($"Input field is empty or missing TMP_InputField, using default: {IP}");
+      }
     }
+    else
+    {
+      tugboat.SetClientAddress(IP);
+      Debug.LogWarning($"ipInputFieldObject is null, using default: {IP}");
+    }
+    tugboat.SetPort(7770);
   }
 
   private void HideMenu()
@@ -50,5 +82,10 @@ public class ConnectionSetup : MonoBehaviour
     {
       mainMenuCanvas.gameObject.SetActive(false);
     }
+  }
+
+  private string GetLocalIPv4() // Only returns first IP, maybe not best to use hardcode for safety sy
+  {
+    return Dns.GetHostEntry(Dns.GetHostName()).AddressList.First(f => f.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToString();
   }
 }
