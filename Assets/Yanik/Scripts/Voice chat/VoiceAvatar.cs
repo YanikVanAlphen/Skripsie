1140c7f111e451c8bc710a74833624366453c96f
Van Alphen <24867276@sun.ac.za>
2025-10-31 10:43:29 +0200

    Added more comments to voiceavatar script to explain chatroom join logic

--- Assets/Yanik/Scripts/Voice chat/VoiceAvatar.cs
+++ Assets/Yanik/Scripts/Voice chat/VoiceAvatar.cs
@@ -9,8 +9,8 @@ namespace FishyVoice
 {
   public class VoiceAvatar : NetworkBehaviour
   {
-    private Agent agent;
-    private const string chatroomName = "<DEFAULT>";
+    private Agent agent; // ref to UniVoice Agent, handles audio input/output and voice data transmission over network
+    private const string chatroomName = "<DEFAULT>"; //  same chatroom name as connection setup's created chat room
 
     public override void OnStartClient()
     {
@@ -20,7 +20,7 @@ namespace FishyVoice
 
     private IEnumerator StartVoiceAgent()
     {
-      // wait for VoiceNetwork instance + network to be ready
+      // wait for VoiceNetwork instance to be created and that the network is actually running
       while (VoiceNetwork.instance == null || !VoiceNetwork.instance.networkActive)
         yield return new WaitForSeconds(0.1f);
       // get created voicenetwork instance now that it is ready
@@ -30,19 +30,20 @@ namespace FishyVoice
       while (LocalConnection.ClientId < 0)
         yield return new WaitForSeconds(0.1f);
 
+      // create a new agent object
       agent = voiceNetwork.CreateAgent();
 
-      // wait for the chatroom to be created/exist
+      // wait for the chatroom to be created/exist, openrooms is a dictionary that contains all active chatrooms and clients in the rooms
       while (!voiceNetwork.openRooms.ContainsKey(chatroomName))
         yield return new WaitForSeconds(0.1f);
-
-      // everyone joins the room
-      voiceNetwork.JoinChatroom(chatroomName); // sync openRooms via ServerRpc
-      agent.JoinChatroom(chatroomName); // route UniVoice audio
+      // adds my clientID to rooms client list and lets other clients know that I joined. Tell server I want to join chat
+      voiceNetwork.JoinChatroom(chatroomName);
+      // UniVoice joining. Start my microphone and get ready to send/receive audio
+      agent.JoinChatroom(chatroomName);
       Debug.Log($"Client {LocalConnection.ClientId} joined chatroom {chatroomName}");
     }
 
-    public override void OnStopClient()
+    public override void OnStopClient() // runs when client instance stops, provided by Fishnetworking
     {
       base.OnStopClient();
 
